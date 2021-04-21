c48f8ef14578f31b1d353890183315b2d7830b10
matt <matte@fastmail.com>
2021-04-21 08:46:32 -0500

    now supports being run as a PWA

---
+++ public/serviceWorker.js
@@ -0,0 +1,1 @@
+//we need this for non-firefox PWAs. we aren't supporting offline because the db isn't catched so it's pointless
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -19,15 +19,23 @@ export default class Header extends React.Component {
 					<div className="col-12 col-md-6 text-center"><h3>RecipeDB</h3></div>
 					<div className="col-12 col-md-6 text-center top-nav">
 						<div>
-						<NavLink to="/" activeClassName="active" isActive={checkActive}>Home</NavLink>
-						<NavLink to="/search" activeClassName="active">Search</NavLink>
-						<NavLink to="/recipe/0" activeClassName="active">Add</NavLink>
-						<NavLink to="/camera" activeClassName="active">Camera</NavLink>
+							<NavLink to="/" activeClassName="active" isActive={checkActive}>Home</NavLink>
+							<NavLink to="/search" activeClassName="active">Search</NavLink>
+							<NavLink to="/recipe/0" activeClassName="active">Add</NavLink>
+							<NavLink to="/camera" activeClassName="active">Camera</NavLink>
 						</div>
 						<div>
-						<NavLink to="/list" activeClassName="active">Shopping List</NavLink>
-						<NavLink to="/upload" activeClassName="active">Upload</NavLink>
-						<NavLink to="/tags" activeClassName="active">Tags</NavLink>
+							<NavLink to="/list" activeClassName="active">Shopping List</NavLink>
+							<NavLink to="/upload" activeClassName="active">Upload</NavLink>
+							<NavLink to="/tags" activeClassName="active">Tags</NavLink>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 text-center">
+						<div className="d-sm-block d-md-none attention">
+							New! You can now add this to your home screen<br />
+							<a href="https://natomasunified.org/kb/add-website-to-mobile-device-home-screen/" target="_new">Learn How</a>
 						</div>
 					</div>
 				</div>
